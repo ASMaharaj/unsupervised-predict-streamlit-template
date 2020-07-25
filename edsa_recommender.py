c37302a220a7c7241df20d27a12483b08fb8caec
Armaan <armaan.maharaj@gmail.com>
2020-07-25 17:02:43 +0200

    Added content for the about us page

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -43,6 +43,9 @@ title_list = load_movie_titles('resources/data/movies.csv')
 # App declaration
 def main():
 
+        st.sidebar.image("https://raw.githubusercontent.com/ASMaharaj/unsupervised-predict-streamlit-template/developing/resources/images/Company%20logo.png",
+                     use_column_width=True)
+
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
     page_options = ["Recommender System","Solution Overview","Data Exploration", "Our Products and Services", "About Us"]
@@ -107,14 +110,89 @@ def main():
     # You may want to add more sections here for aspects such as an EDA,
     # or to provide your business pitch.
 
+
+    # Data Exploration Page
     if page_selection == "Data Exploration":
         st.image("https://raw.githubusercontent.com/ASMaharaj/unsupervised-predict-streamlit-template/developing/resources/images/DataExploration.png", use_column_width=True)
 
+    # Our Products and Services Page
     if page_selection == "Our Products and Services":
         st.image("https://raw.githubusercontent.com/ASMaharaj/unsupervised-predict-streamlit-template/developing/resources/images/OurProductsServices.png", use_column_width=True)
 
+    # About Us Page
     if page_selection == "About Us":
-        st.image("https://raw.githubusercontent.com/ASMaharaj/unsupervised-predict-streamlit-template/developing/resources/images/About%20Us.png", use_column_width=True)
+        st.image("https://raw.githubusercontent.com/ASMaharaj/unsupervised-predict-streamlit-template/developing/resources/images/About%20Us.png",
+        use_column_width=True)
+
+        # table of contents on the sidebar
+        st.sidebar.title("Table of contents")
+        st.sidebar.info("The table of contents is interactive")
+        class Toc3:
+            def __init__(self):
+                self._items = []
+                self._placeholder = None
+
+            def title(self, text):
+                self._markdown(text, "h1")
+
+            def header(self, text):
+                self._markdown(text, "h2", " " * 2)
+
+            def subheader(self, text):
+                self._markdown(text, "h3", " " * 4)
+
+            def placeholder(self, sidebar = False ):
+                self._placeholder = st.sidebar
+
+            def generate(self):
+                if self._placeholder:
+                    self._placeholder.markdown("\n".join(self._items), unsafe_allow_html=True)
+
+            def _markdown(self, text, level, space=""):
+                key = "".join(filter(str.isalnum, text)).lower()
+
+                st.markdown(f"<{level} id='{key}'>{text}</{level}>", unsafe_allow_html=True)
+                self._items.append(f"{space}* <a href='#{key}'>{text}</a>")
+
+
+        toc3 = Toc3()
+
+        toc3.placeholder()
+
+
+        # Our Company background story
+        toc3.title("OUR STORY")
+        st.markdown("""
+                    Net Flieks started in the Summer of 2019.
+                    A group of individuals came together with the same vision:
+                            Create recommender systems for content that users would find interesting.
+                    A concept now brought to perfection by Net flieks of creators.
+                    Today, OneFlix are helping users in selecting various content that peaks their interests and exposing them to sources and sites that they would normally overlook which
+                    could be of interest for them.
+                    Net Flieks features a variety of recommender systems that users can make use of, using a broad spectrum of data preprocessing techniques and parameters.
+                    Simply put: There's content for every user, mindset and style.
+                    """)
+
+        # Content creators who worked on this assignment
+        toc3.title("MEET THE TEAM")
+
+        toc3.header("Armaan Singh Maharaj")
+        #st.markdown("")
+        st.image("https://raw.githubusercontent.com/ASMaharaj/unsupervised-predict-streamlit-template/developing/resources/images/Armaan.jpeg",
+        caption = "Have you ever seen me and Batman in the same room😉", use_column_width =True)
+
+        toc3.header("Primrose Mukhethwa Ramukhuvhathi")
+
+        toc3.header("Abednego Pakaree")
+
+        toc3.header("netsianda khathutshelo")
+
+        toc3.header("Ntokozo Nkanyane")
+
+        toc3.header("Tsundukani Makhubela")
+
+        toc3.generate()
+
 
 
 if __name__ == '__main__':
