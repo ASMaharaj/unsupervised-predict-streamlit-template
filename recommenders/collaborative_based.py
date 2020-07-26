60c911766948dd22fffed5ebed4e5a956c5981a7
Armaan <armaan.maharaj@gmail.com>
2020-07-26 18:57:42 +0200

    fixed indentation

--- recommenders/collaborative_based.py
+++ recommenders/collaborative_based.py
@@ -51,145 +51,145 @@ movieid_to_title_df = movieid_to_title_df.drop_duplicates()
 movieid_to_title_df.dropna(inplace = True)
 
 def prediction_item(item_id):
-"""Map a given favourite movie to users within the
-MovieLens dataset with the same preference.
+    """ Map a given favourite movie to users within the
+    MovieLens dataset with the same preference.
 
-Parameters
-----------
-item_id : int
-A MovieLens Movie ID.
+    Parameters
+    ----------
+    item_id : int
+    A MovieLens Movie ID.
 
-Returns
--------
-list
-User IDs of users with similar high ratings for the given movie.
+    Returns
+    -------
+    list
+    User IDs of users with similar high ratings for the given movie.
 
-"""
-# Data preprosessing
-reader = Reader(rating_scale=(0, 5))
-load_df = Dataset.load_from_df(train[['userId','movieId','rating']],reader)
-a_train = load_df.build_full_trainset()
+    """
+    # Data preprosessing
+    reader = Reader(rating_scale=(0, 5))
+    load_df = Dataset.load_from_df(train[['userId','movieId','rating']],reader)
+    a_train = load_df.build_full_trainset()
 
-predictions = []
-for ui in a_train.all_users():
-predictions.append(model.predict(iid=item_id,uid=ui, verbose = False))
-return predictions
+    predictions = []
+    for ui in a_train.all_users():
+    predictions.append(model.predict(iid=item_id,uid=ui, verbose = False))
+    return predictions
 
 def pred_movies(movie_list):
-"""Maps the given favourite movies selected within the app to corresponding
-users within the MovieLens dataset.
+    """Maps the given favourite movies selected within the app to corresponding
+    users within the MovieLens dataset.
 
-Parameters
-----------
-movie_list : list
-Three favourite movies selected by the app user.
+    Parameters
+    ----------
+    movie_list : list
+    Three favourite movies selected by the app user.
 
-Returns
--------
-list
-User-ID's of users with similar high ratings for each movie.
+    Returns
+    -------
+    list
+    User-ID's of users with similar high ratings for each movie.
 
-"""
-# Store the id of users
-id_store=[]
-# For each movie selected by a user of the app,
-# predict a corresponding user within the dataset with the highest rating
-for i in movie_list:
-#movie_id = movieid_to_title_df[movieid_to_title_df['title'] == i]['movieId'].values[0]
-predictions = prediction_item(item_id = i)
-predictions.sort(key=lambda x: x.est, reverse=True)
-# Take the top 10 user id's from each movie with highest rankings
-for pred in predictions[:10]:
-id_store.append(pred.uid)
-# Return a list of user id's
-return id_store
+    """
+    # Store the id of users
+    id_store=[]
+    # For each movie selected by a user of the app,
+    # predict a corresponding user within the dataset with the highest rating
+    for i in movie_list:
+    #movie_id = movieid_to_title_df[movieid_to_title_df['title'] == i]['movieId'].values[0]
+    predictions = prediction_item(item_id = i)
+    predictions.sort(key=lambda x: x.est, reverse=True)
+    # Take the top 10 user id's from each movie with highest rankings
+    for pred in predictions[:10]:
+    id_store.append(pred.uid)
+    # Return a list of user id's
+    return id_store
 
 def prediction(movie_title,user_id):
-"""Predicts users rating based on movie
+    """Predicts users rating based on movie
 
-Parameters
-----------
-movie_title : string
-The name of the movie
-user_id : integer
-The user identification
+    Parameters
+    ----------
+    movie_title : string
+    The name of the movie
+    user_id : integer
+    The user identification
 
-Returns
--------
-value
-The predicted rating based on movie and user
+    Returns
+    -------
+    value
+    The predicted rating based on movie and user
 
-"""
-#movie_id = movieid_to_title_df[movieid_to_title_df['title'] == movie_title]['movieId'].values[0]
-predictions = model.predict(iid=movie_title,uid=user_id, verbose = False)
-return predictions.est
+    """
+    #movie_id = movieid_to_title_df[movieid_to_title_df['title'] == movie_title]['movieId'].values[0]
+    predictions = model.predict(iid=movie_title,uid=user_id, verbose = False)
+    return predictions.est
 
 # !! DO NOT CHANGE THIS FUNCTION SIGNATURE !!
 # You are, however, encouraged to change its content.
 def collab_model(movie_list,top_n=10):
-"""Performs Collaborative filtering based upon a list of movies supplied
-by the app user.
+    """Performs Collaborative filtering based upon a list of movies supplied
+    by the app user.
 
-Parameters
-----------
-movie_list : list (str)
-Favorite movies chosen by the app user.
-top_n : type
-Number of top recommendations to return to the user.
+    Parameters
+    ----------
+    movie_list : list (str)
+    Favorite movies chosen by the app user.
+    top_n : type
+    Number of top recommendations to return to the user.
 
-Returns
--------
-list (str)
-Titles of the top-n movie recommendations to the user.
+    Returns
+    -------
+    list (str)
+    Titles of the top-n movie recommendations to the user.
 
-"""
-movie_ids = pred_movies(movie_list)
-print('first phase')
-df_init_users = train[train['userId']==movie_ids[0]]
-for i in movie_ids[1:]:
-df_init_users=df_init_users.append(train[train['userId']==i])
-# Getting the user-item matrix
-df_init_users = pd.merge(df_init_users, movieid_to_title_df, on = 'movieId', how = 'left')
-df_init_users = df_init_users.dropna()
-top_users_matrix = df_init_users.groupby(['title','userId'])['rating'].max().unstack()
+    """
+    movie_ids = pred_movies(movie_list)
+    print('first phase')
+    df_init_users = train[train['userId']==movie_ids[0]]
+    for i in movie_ids[1:]:
+    df_init_users=df_init_users.append(train[train['userId']==i])
+    # Getting the user-item matrix
+    df_init_users = pd.merge(df_init_users, movieid_to_title_df, on = 'movieId', how = 'left')
+    df_init_users = df_init_users.dropna()
+    top_users_matrix = df_init_users.groupby(['title','userId'])['rating'].max().unstack()
 
-# Adding the movies from list into user-item matrix if not there
-for i in movie_list:
-if i not in top_users_matrix.index.values.tolist():
-length = len(top_users_matrix.columns)
-df_nan = pd.DataFrame([[(np.NaN)]*length], index = [i], columns = top_users_matrix.columns)
-top_users_matrix = top_users_matrix.append(df_nan)
+    # Adding the movies from list into user-item matrix if not there
+    for i in movie_list:
+    if i not in top_users_matrix.index.values.tolist():
+    length = len(top_users_matrix.columns)
+    df_nan = pd.DataFrame([[(np.NaN)]*length], index = [i], columns = top_users_matrix.columns)
+    top_users_matrix = top_users_matrix.append(df_nan)
 
-# makinf predictions for those movies based on algorithm
-for i in top_users_matrix.columns:
-for j in top_users_matrix.index.values.tolist():
-if np.isnan(top_users_matrix[i].loc[j]):
-top_users_matrix[i].loc[j] = prediction(j,i)
+    # makinf predictions for those movies based on algorithm
+    for i in top_users_matrix.columns:
+    for j in top_users_matrix.index.values.tolist():
+    if np.isnan(top_users_matrix[i].loc[j]):
+    top_users_matrix[i].loc[j] = prediction(j,i)
 
-# Getting the cosine similarity matrix
-cosine_sim = cosine_similarity(top_users_matrix)
-indices = pd.Series(top_users_matrix.index)
-idx_1 = indices[indices == movie_list[0]].index[0]
-idx_2 = indices[indices == movie_list[1]].index[0]
-idx_3 = indices[indices == movie_list[2]].index[0]
+    # Getting the cosine similarity matrix
+    cosine_sim = cosine_similarity(top_users_matrix)
+    indices = pd.Series(top_users_matrix.index)
+    idx_1 = indices[indices == movie_list[0]].index[0]
+    idx_2 = indices[indices == movie_list[1]].index[0]
+    idx_3 = indices[indices == movie_list[2]].index[0]
 
-# Creating a Series with the similarity scores in descending order
-rank_1 = cosine_sim[idx_1]
-rank_2 = cosine_sim[idx_2]
-rank_3 = cosine_sim[idx_3]
+    # Creating a Series with the similarity scores in descending order
+    rank_1 = cosine_sim[idx_1]
+    rank_2 = cosine_sim[idx_2]
+    rank_3 = cosine_sim[idx_3]
 
-# Calculating the scores
-score_series_1 = pd.Series(rank_1).sort_values(ascending = False)
-score_series_2 = pd.Series(rank_2).sort_values(ascending = False)
-score_series_3 = pd.Series(rank_3).sort_values(ascending = False)
+    # Calculating the scores
+    score_series_1 = pd.Series(rank_1).sort_values(ascending = False)
+    score_series_2 = pd.Series(rank_2).sort_values(ascending = False)
+    score_series_3 = pd.Series(rank_3).sort_values(ascending = False)
 
-# Appending the names of movies
-listings = score_series_1.append(score_series_2).append(score_series_3).sort_values(ascending = False)
-recommended_movies = []
-# Choose top 50
-top_50_indexes = list(listings.iloc[1:50].index)
-# Removing chosen movies
-top_indexes = np.setdiff1d(top_50_indexes,[idx_1,idx_2,idx_3])
-for i in top_indexes[:top_n]:
-recommended_movies.append(indices[i])
-return recommended_movies
+    # Appending the names of movies
+    listings = score_series_1.append(score_series_2).append(score_series_3).sort_values(ascending = False)
+    recommended_movies = []
+    # Choose top 50
+    top_50_indexes = list(listings.iloc[1:50].index)
+    # Removing chosen movies
+    top_indexes = np.setdiff1d(top_50_indexes,[idx_1,idx_2,idx_3])
+    for i in top_indexes[:top_n]:
+    recommended_movies.append(indices[i])
+    return recommended_movies
